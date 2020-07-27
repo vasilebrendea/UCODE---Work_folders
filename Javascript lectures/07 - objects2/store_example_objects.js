37a08ec8a6edfa7d5c72aaa4662379dc3d2f1f8e
vasileBrendea <vasybrendea@gmail.com>
2020-07-27 22:35:18 +0300

    4 exercise from ex. 7

---
+++ Javascript lectures/07 - objects2/store_example_objects.js
@@ -0,0 +1,251 @@
+
+// 2. Please write a system for a store. 
+// The store have n shelf.
+// every shelf can have the following settings
+// 	- color
+// 	- light intensity
+
+// 	- type
+// 		- sport
+// 		- toys
+// 		- jewelry
+// 		- clothes
+// 		- presentation (the products from this shelf cannot be sold and has no price)
+// 	- allowedProductNumbers - how many products can fit into the shelf
+
+// when the color or light intensity is changed, this setting it will be aplied to all shelf
+
+// On a shelf you can put maximum "allowedProductNumbers" products;
+
+// every product has a 
+// 	- cost
+// 	- profit;
+// 	- price = cost + profit
+// 	- code; generate 5 random characters
+// 	- type (same as shelf type)
+
+
+// Design a system like this
+// the store can:
+// 	receive a product; it will be added on a shelf; if there is no shelf, it will return false
+// 	provide the sum of all products 
+// 	provide the sum of all products for every category
+// 	sell a product with a specific code
+// 	add more shelfs
+// 	reserve a product for a period; in this case the product cannot be sold
+// 	the store will keep a history of sold products
+
+// 3. create another store with exact data from 2; 
+
+
+// magazin - literal objects
+//     - n rafturi == > shelfs: []
+//         - adauga raft
+//         - sterge raft
+//         - modifica raft
+
+
+
+//REGULA: fiecare obiect e recomandat sa aiba un GUID
+
+
+// class Helper {
+//     getRandomString() {
+//         return Math.random().toString(36).substring(7);
+//     }
+// }
+
+var  Helper = {
+    getRandomString: function() {
+        return Math.random().toString(36).substring(7);
+    }
+}
+
+
+var settings = {
+    color: 'red',
+    lightIntensity : 10,
+} 
+
+var shelfType = {
+    notSET: 0,
+    sport: 1,
+    toys: 2,
+    jewelry: 4,
+    clothes: 8,
+};
+
+// bitwise operators
+
+function shelf() {
+    this.id = Helper.getRandomString();
+    this.type = shelfType.notSET;
+    this.settings = settings;
+    this.allowedProductNumbers = 5;
+    this.setProductsNumber = function(n) {
+        this.allowedProductNumbers = n;
+    }
+
+    this.addProduct = function(shelf) {
+
+    }
+    this.removeProduct = function(shelf) {
+
+    }
+    this.updateProduct =function(id, product) {
+
+    }
+
+    return this;
+}
+
+function product() {
+    this.id = Helper.getRandomString();
+    this.type = shelfType.notSET;
+    var cost = 0;
+    var profit = 0;
+
+    this.price = cost + profit;
+   
+
+    return this;
+}
+
+class productAsClass {
+    id = Helper.getRandomString();
+    type = shelfType.notSET;
+    cost = 0;
+    profit = 0;
+
+    price = 0;
+
+    constructor() {
+        debugger;
+        this.price = this.cost + this.profit;
+    }
+}
+
+
+//v1
+var productManager = {
+    create: function(data) {
+        const prod = {
+            id: Helper.getRandomString(),
+            type: shelfType.jewelry
+        };
+
+        prod.cost = data.cost;
+
+        return prod;
+    }
+}; 
+
+
+//v2
+function productManager1(data){
+
+        var doSomething = function() {
+            debugger;
+        }
+
+        const prod = {
+            id: Helper.getRandomString(),
+            type: shelfType.jewelry,
+            test: function() {
+                debugger;
+                this.doSomething();
+            }
+        };
+
+        prod.cost = data.cost;
+
+        return prod;
+}; 
+
+
+function productManager1Closure(data){
+
+    this.doSomething = function() {
+        debugger;
+    }
+
+    const prod = {
+        id: Helper.getRandomString(),
+        type: shelfType.jewelry,
+        test: () => {
+            debugger;
+            this.doSomething();
+        }
+    };
+
+    prod.cost = data.cost;
+
+    return prod;
+}; 
+
+function productManager2(data){
+
+    this.doSomething = function() {
+        debugger;
+    }
+
+    const prod = {
+        id: Helper.getRandomString(),
+        type: shelfType.jewelry,
+        test: function(){
+            debugger;
+            this.doSomething();
+        }
+    };
+
+    prod.cost = data.cost;
+
+    prod.test = prod.test.bind(this);
+
+    return prod;
+}; 
+
+var store = {
+    shelfs: [],
+    shelfService: {
+        // shelfs: [],
+        addShelf: function(shelf) {
+            store.shelfs.push(shelf);
+        },
+        removeShelf: function(shelf) {
+            store.shelfs = store.shelfs.filter(el => el.id !== shelf.id);
+        },
+        updateShelf: function(id, shelf) {
+    
+        },
+    },
+    productService: new genericList,
+    // shelfService: new genericList,
+};
+ function genericList()  {
+    this.items =[];
+    this.add = function(it) {
+        this.items.push(it);        
+    }
+
+}
+
+
+debugger;
+var r1 = new shelf();
+r1.type = shelfType.sport;
+
+var r2 = new shelf();
+r2.type = shelfType.clothes;
+
+store.shelfService.addShelf(r1);
+store.shelfService.addShelf(r2);
+
+
+settings.color = 'yellow';
+
+var r3 = new shelf();
+r3.type = shelfType.jewelry;
+store.shelfService.addShelf(r3);
+
+debugger; 
---
+++ template/template/munca_07.js
@@ -0,0 +1,144 @@
+// 1. WAF WR 6 random numbers out of 49
+
+// let generate6Numbers = {
+//   numbers: [],
+// };
+
+// function generatingNumbers() {
+//   for (let i = 0; i < 6; i++) {
+//     let generateNumbers = Math.floor(Math.random() * 49 + 1);
+//     generate6Numbers.numbers.push(generateNumbers);
+//   }
+// }
+// generatingNumbers();
+
+// console.log(generate6Numbers.numbers);
+
+// 2. simulate a 6/49 game. you have the winning 6 numbers and n tickets. return the tickets which has 1 winning number, 2 , .... 6
+
+// let ticketOfNumbers = {
+//   name: "Vasile",
+//   numbers: [2, 3, 22, 31, 49, 37],
+// };
+
+// let game6_49 = {
+//   numbersFromTicket: [],
+//   ramdomNumbers: [],
+//   winnigTicket: "",
+// };
+
+// function tickets() {
+//   ticketOfNumbers.numbers.map(function (num) {
+//     if (num > 0 && num <= 49) {
+//       game6_49.numbersFromTicket.push(num);
+//     } else {
+//       console.log("The numbers must be from 1 to 49 !!!");
+//     }
+//   });
+// }
+// tickets();
+
+// function generatingNumbers() {
+//   for (let i = 0; i < 6; i++) {
+//     let generateNumbers = Math.floor(Math.random() * 49 + 1);
+//     for (let x = 0; x < game6_49.ramdomNumbers.length; x++) {
+//       if (x == i) {
+//         continue;
+//       } else if (game6_49.ramdomNumbers[i] == game6_49.ramdomNumbers[x]) {
+//         game6_49.ramdomNumbers[i] = Math.floor(Math.random() * 49 + 1);
+//       }
+//     }
+//     game6_49.ramdomNumbers.push(generateNumbers);
+//   }
+// }
+// generatingNumbers();
+
+// function winnigTicket() {
+//   let winningNumbers = [];
+//   game6_49.numbersFromTicket.map(function (win) {
+//     game6_49.ramdomNumbers.map(function (random) {
+//       if (win == random) {
+//         winningNumbers.push(win);
+//         console.log("The winning numbers are " + winningNumbers + " !!!");
+//         console.log(
+//           "The winning ticket belongs to " + ticketOfNumbers.name + "."
+//         );
+//         game6_49.winnigTicket = ticketOfNumbers.name;
+//       }
+//     });
+//   });
+// }
+// winnigTicket();
+// console.log(game6_49);
+
+//3. Create a chat. you have one chat room with many users.
+
+// let allMessages = [];
+
+// class newUser {
+//   constructor(name, msg) {
+//     this.name = name;
+//     this.userMessages = [msg];
+//   }
+//   addMessage(message) {
+//     this.userMessages.push(message);
+//   }
+// }
+
+// function pushUser(usr) {
+//   allMessages.push(usr);
+// }
+
+// function seeUser(userName) {
+//   allMessages.map(function (element) {
+//     if (userName === element.name) {
+//       console.log(element.name + " messages are :");
+//       console.log(element.userMessages);
+//     }
+//   });
+// }
+
+// function displayAllMessages() {
+//   let allMsgArr = [];
+//   allMessages.forEach(function (element) {
+//     allMsgArr.push(...element.userMessages);
+//     element.allUsersMessages = allMsgArr;
+//   });
+// }
+
+// const user1 = new newUser("Vasile", "a");
+// pushUser(user1);
+// user1.addMessage("b");
+
+// const user2 = new newUser("Daniel", "c");
+// pushUser(user2);
+// user2.addMessage("d");
+// user2.addMessage("hello");
+
+// const user3 = new newUser("Adi", "de");
+// pushUser(user3);
+// user3.addMessage("are");
+
+// // seeUser("Adi");
+
+// displayAllMessages();
+// console.log(allMessages);
+
+// 4. you have an array; please return all pairs of numbers with sum equal n
+// ex: [1,3,2,6,5], 7 ==> [[1,6],[2,5]]
+
+// function sumPairOf(arr, num) {
+//   let array = [];
+//   arr.map((el) => {
+//     arr.map((element) => {
+//       if (el + element === num) {
+//         array.push([el, element]);
+//       }
+//     });
+//   });
+//   console.log(array);
+// }
+
+// sumPairOf([1, 3, 2, 6, 5], 7);
+
+// 5. please design a matrix of n/n numbers containing random numbers between (0, n)
