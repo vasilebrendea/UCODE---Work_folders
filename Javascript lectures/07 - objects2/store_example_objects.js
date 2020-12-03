035248b087e29e50037a2e9563d40d1761fcbad0
vasileBrendea <vasybrendea@gmail.com>
2020-12-03 22:07:42 +0200

    modificare fisier

--- Javascript lectures/07 - objects2/store_example_objects.js
+++ Javascript lectures/07 - objects2/store_example_objects.js
@@ -1,5 +1,4 @@
-
-// 2. Please write a system for a store. 
+// 2. Please write a system for a store.
 // The store have n shelf.
 // every shelf can have the following settings
 // 	- color
@@ -17,26 +16,24 @@
 
 // On a shelf you can put maximum "allowedProductNumbers" products;
 
-// every product has a 
+// every product has a
 // 	- cost
 // 	- profit;
 // 	- price = cost + profit
 // 	- code; generate 5 random characters
 // 	- type (same as shelf type)
 
-
 // Design a system like this
 // the store can:
 // 	receive a product; it will be added on a shelf; if there is no shelf, it will return false
-// 	provide the sum of all products 
+// 	provide the sum of all products
 // 	provide the sum of all products for every category
 // 	sell a product with a specific code
 // 	add more shelfs
 // 	reserve a product for a period; in this case the product cannot be sold
 // 	the store will keep a history of sold products
 
-// 3. create another store with exact data from 2; 
-
+// 3. create another store with exact data from 2;
 
 // magazin - literal objects
 //     - n rafturi == > shelfs: []
@@ -44,10 +41,7 @@
 //         - sterge raft
 //         - modifica raft
 
-
-
 //REGULA: fiecare obiect e recomandat sa aiba un GUID
-
 
 // class Helper {
 //     getRandomString() {
@@ -55,181 +49,163 @@
 //     }
 // }
 
-var  Helper = {
-    getRandomString: function() {
-        return Math.random().toString(36).substring(7);
-    }
-}
+// var  Helper = {
+//     getRandomString: function() {
+//         return Math.random().toString(36).substring(7);
+//     }
+// }
 
+// var settings = {
+//     color: 'red',
+//     lightIntensity : 10,
+// }
 
-var settings = {
-    color: 'red',
-    lightIntensity : 10,
-} 
-
-var shelfType = {
-    notSET: 0,
-    sport: 1,
-    toys: 2,
-    jewelry: 4,
-    clothes: 8,
-};
+// var shelfType = {
+//     notSET: 0,
+//     sport: 1,
+//     toys: 2,
+//     jewelry: 4,
+//     clothes: 8,
+// };
 
 // bitwise operators
 
 function shelf() {
-    this.id = Helper.getRandomString();
-    this.type = shelfType.notSET;
-    this.settings = settings;
-    this.allowedProductNumbers = 5;
-    this.setProductsNumber = function(n) {
-        this.allowedProductNumbers = n;
-    }
+  this.id = Helper.getRandomString();
+  this.type = shelfType.notSET;
+  this.settings = settings;
+  this.allowedProductNumbers = 5;
+  this.setProductsNumber = function (n) {
+    this.allowedProductNumbers = n;
+  };
 
-    this.addProduct = function(shelf) {
+  this.addProduct = function (shelf) {};
+  this.removeProduct = function (shelf) {};
+  this.updateProduct = function (id, product) {};
 
-    }
-    this.removeProduct = function(shelf) {
-
-    }
-    this.updateProduct =function(id, product) {
-
-    }
-
-    return this;
+  return this;
 }
 
 function product() {
-    this.id = Helper.getRandomString();
-    this.type = shelfType.notSET;
-    var cost = 0;
-    var profit = 0;
+  this.id = Helper.getRandomString();
+  this.type = shelfType.notSET;
+  var cost = 0;
+  var profit = 0;
 
-    this.price = cost + profit;
-   
+  this.price = cost + profit;
 
-    return this;
+  return this;
 }
 
 class productAsClass {
-    id = Helper.getRandomString();
-    type = shelfType.notSET;
-    cost = 0;
-    profit = 0;
+  id = Helper.getRandomString();
+  type = shelfType.notSET;
+  cost = 0;
+  profit = 0;
 
-    price = 0;
+  price = 0;
 
-    constructor() {
-        debugger;
-        this.price = this.cost + this.profit;
-    }
+  constructor() {
+    debugger;
+    this.price = this.cost + this.profit;
+  }
 }
-
 
 //v1
 var productManager = {
-    create: function(data) {
-        const prod = {
-            id: Helper.getRandomString(),
-            type: shelfType.jewelry
-        };
+  create: function (data) {
+    const prod = {
+      id: Helper.getRandomString(),
+      type: shelfType.jewelry,
+    };
 
-        prod.cost = data.cost;
+    prod.cost = data.cost;
 
-        return prod;
-    }
-}; 
-
+    return prod;
+  },
+};
 
 //v2
-function productManager1(data){
+function productManager1(data) {
+  var doSomething = function () {
+    debugger;
+  };
 
-        var doSomething = function() {
-            debugger;
-        }
-
-        const prod = {
-            id: Helper.getRandomString(),
-            type: shelfType.jewelry,
-            test: function() {
-                debugger;
-                this.doSomething();
-            }
-        };
-
-        prod.cost = data.cost;
-
-        return prod;
-}; 
-
-
-function productManager1Closure(data){
-
-    this.doSomething = function() {
-        debugger;
-    }
-
-    const prod = {
-        id: Helper.getRandomString(),
-        type: shelfType.jewelry,
-        test: () => {
-            debugger;
-            this.doSomething();
-        }
-    };
-
-    prod.cost = data.cost;
-
-    return prod;
-}; 
-
-function productManager2(data){
-
-    this.doSomething = function() {
-        debugger;
-    }
-
-    const prod = {
-        id: Helper.getRandomString(),
-        type: shelfType.jewelry,
-        test: function(){
-            debugger;
-            this.doSomething();
-        }
-    };
-
-    prod.cost = data.cost;
-
-    prod.test = prod.test.bind(this);
-
-    return prod;
-}; 
-
-var store = {
-    shelfs: [],
-    shelfService: {
-        // shelfs: [],
-        addShelf: function(shelf) {
-            store.shelfs.push(shelf);
-        },
-        removeShelf: function(shelf) {
-            store.shelfs = store.shelfs.filter(el => el.id !== shelf.id);
-        },
-        updateShelf: function(id, shelf) {
-    
-        },
+  const prod = {
+    id: Helper.getRandomString(),
+    type: shelfType.jewelry,
+    test: function () {
+      debugger;
+      this.doSomething();
     },
-    productService: new genericList,
-    // shelfService: new genericList,
-};
- function genericList()  {
-    this.items =[];
-    this.add = function(it) {
-        this.items.push(it);        
-    }
+  };
 
+  prod.cost = data.cost;
+
+  return prod;
 }
 
+function productManager1Closure(data) {
+  this.doSomething = function () {
+    debugger;
+  };
+
+  const prod = {
+    id: Helper.getRandomString(),
+    type: shelfType.jewelry,
+    test: () => {
+      debugger;
+      this.doSomething();
+    },
+  };
+
+  prod.cost = data.cost;
+
+  return prod;
+}
+
+function productManager2(data) {
+  this.doSomething = function () {
+    debugger;
+  };
+
+  const prod = {
+    id: Helper.getRandomString(),
+    type: shelfType.jewelry,
+    test: function () {
+      debugger;
+      this.doSomething();
+    },
+  };
+
+  prod.cost = data.cost;
+
+  prod.test = prod.test.bind(this);
+
+  return prod;
+}
+
+var store = {
+  shelfs: [],
+  shelfService: {
+    // shelfs: [],
+    addShelf: function (shelf) {
+      store.shelfs.push(shelf);
+    },
+    removeShelf: function (shelf) {
+      store.shelfs = store.shelfs.filter((el) => el.id !== shelf.id);
+    },
+    updateShelf: function (id, shelf) {},
+  },
+  productService: new genericList(),
+  // shelfService: new genericList,
+};
+function genericList() {
+  this.items = [];
+  this.add = function (it) {
+    this.items.push(it);
+  };
+}
 
 debugger;
 var r1 = new shelf();
@@ -241,11 +217,10 @@ r2.type = shelfType.clothes;
 store.shelfService.addShelf(r1);
 store.shelfService.addShelf(r2);
 
-
-settings.color = 'yellow';
+settings.color = "yellow";
 
 var r3 = new shelf();
 r3.type = shelfType.jewelry;
 store.shelfService.addShelf(r3);
 
-debugger; 
+debugger;
