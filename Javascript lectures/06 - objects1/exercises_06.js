21e15da56f2c4bf0060fcb8b7695c20aa4adafee
vasileBrendea <vasybrendea@gmail.com>
2020-12-03 22:36:24 +0200

    comment

--- Javascript lectures/06 - objects1/exercises_06.js
+++ Javascript lectures/06 - objects1/exercises_06.js
@@ -1,63 +1,50 @@
-this
-closure
+// this
+// closure
 
-- take a look over the javascript closure from documentation
+// - take a look over the javascript closure from documentation
 
+// // 1. please waf prototype whcj implements the map BUT not returns another instance of the array; mymap
 
-// 1. please waf prototype whcj implements the map BUT not returns another instance of the array; mymap
+// // 	- [1,2,3] ==> [1,4,9]
+// // 	arr1 --> arr2
 
-// 	- [1,2,3] ==> [1,4,9] 
-// 	arr1 --> arr2
+// // 	arr1 --> arr1 [1,4,9]
 
-// 	arr1 --> arr1 [1,4,9]
+// // Array.prototype.mymap ....
 
+// 2. Please write a system for a store.
+// The store have n shelf.
+// every shelf can have the following settings
+// 	- color
+// 	- light intensity
 
-// Array.prototype.mymap ....
+// 	- type
+// 		- sport
+// 		- toys
+// 		- jewelry
+// 		- clothes
+// 		- presentation (the products from this shelf cannot be sold and has no price)
+// 	- allowedProductNumbers - how many products can fit into the shelf
 
+// when the color or light intensity is changed, this setting it will be aplied to all shelf
 
-2. Please write a system for a store. 
-The store have n shelf.
-every shelf can have the following settings
-	- color
-	- light intensity
+// On a shelf you can put maximum "allowedProductNumbers" products;
 
-	- type
-		- sport
-		- toys
-		- jewelry
-		- clothes
-		- presentation (the products from this shelf cannot be sold and has no price)
-	- allowedProductNumbers - how many products can fit into the shelf
+// every product has a
+// 	- cost
+// 	- profit;
+// 	- price = cost + profit
+// 	- code; generate 5 random characters
+// 	- type (same as shelf type)
 
-when the color or light intensity is changed, this setting it will be aplied to all shelf
+// Design a system like this
+// the store can:
+// 	receive a product; it will be added on a shelf; if there is no shelf, it will return false
+// 	provide the sum of all products
+// 	provide the sum of all products for every category
+// 	sell a product with a specific code
+// 	add more shelfs
+// 	reserve a product for a period; in this case the product cannot be sold
+// 	the store will keep a history of sold products
 
-On a shelf you can put maximum "allowedProductNumbers" products;
-
-every product has a 
-	- cost
-	- profit;
-	- price = cost + profit
-	- code; generate 5 random characters
-	- type (same as shelf type)
-
-
-Design a system like this
-the store can:
-	receive a product; it will be added on a shelf; if there is no shelf, it will return false
-	provide the sum of all products 
-	provide the sum of all products for every category
-	sell a product with a specific code
-	add more shelfs
-	reserve a product for a period; in this case the product cannot be sold
-	the store will keep a history of sold products
-
-3. create another store with exact data from 2; 
-
-		
-
-
-	
-
-
-
-
+// 3. create another store with exact data from 2;
